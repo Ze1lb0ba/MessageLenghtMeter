c2642f4f566c327ae060b7ad62d62a193fc0d7b8
Ze1lb0ba <stankevich_dg@school629.ru>
2022-12-23 13:15:49 +0300

    Добавьте файлы проекта.

---
+++ MessageLenghtMeter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+public partial class Program
+{
+    public static async Task Main()
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+
+        // Объект, отвечающий за постоянный жизненный цикл приложения
+        var host = new HostBuilder()
+            .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+            .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+            .Build(); // Собираем
+
+        Console.WriteLine("Сервис запущен");
+        // Запускаем сервис
+        await host.RunAsync();
+        Console.WriteLine("Сервис остановлен");
+    }
+
+    static void ConfigureServices(IServiceCollection services)
+    {
+        // Регистрируем объект TelegramBotClient c токеном подключения
+        services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("5812807752:AAGhfpOnhbm0uUGCXkqS4djixHuIn2nlXV8"));
+        // Регистрируем постоянно активный сервис бота
+        services.AddHostedService<Bot>();
+    }
+}
